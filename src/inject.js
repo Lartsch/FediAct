20ac4961ce2023e8f10d20783f6237aee9bca482
Lartsch <lartschey@googlemail.com>
2022-12-03 01:59:32 +0100

    Remove unused geturlparameter function

--- src/inject.js
+++ src/inject.js
@@ -111,19 +111,6 @@ async function makeRequest(method, url, extraheaders) {
     });
 }
 
-// extract given url parameter value
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'), sParameterName, i;
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
-    }
-    return false;
-};
-
 function escapeRegExp(string) {
 	return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
   }
