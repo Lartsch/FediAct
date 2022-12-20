76f0369816fcbd8fde7496d93145fba6f8df41ae
lartsch <lartschey@gmail.com>
2022-12-20 07:57:53 -0500

    update packer

--- pack.sh
+++ pack.sh
@@ -1,5 +1,6 @@
 # RUN FROM MAIN DIR
 # USE ./pack.sh <VERSION>
+# NOTE: requires uglifyjs and uglifycss in PATH
 
 # remove all minified files (excluding lib folder) and zip files
 find . -type d -name lib -prune -o -type f -name "*.min.js" -o -type f -name "*.min.css" -o -type f -name "*.zip" | grep -v "/lib" | tr '\n' '\0' | xargs -r0 rm
@@ -8,5 +9,5 @@ find . -type d -name lib -prune -o -type f -name "*.js" -exec bash -c 'uglifyjs 
 # minify all css files, excluding lib folder
 find . -type d -name lib -prune -o -type f -name "*.css" -exec bash -c 'uglifycss $1 > "$(echo $1 | rev | cut -f 2- -d '.' | rev).min.css"' _ {} \;
 # zip release files
-zip -r "fediact-$1-chrome.zip" ./ -x "firefox/*" -x "img/*" -x ".git/*" -x "README.md" -x "NOTES.md" -x ".gitignore" -x "pack.sh" -x "LICENSE" -x "src/background.js" -x "src/inject.js" -x "src/popup.js" -x "src/styles.css" 1>/dev/null
-cd firefox && zip -r "../fediact-$1-firefox.zip" ./ -x "src/background.js" -x "src/inject.js" -x "src/popup.js" -x "src/styles.css" 1>/dev/null
+zip -r "fediact-$1-chrome.zip" ./ -x "firefox/*" -x "img/*" -x ".git/*" -x "README.md" -x "NOTES.md" -x ".gitignore" -x "pack.sh" -x "LICENSE" -x "src/background.js" -x "src/inject.js" -x "src/popup.js" -x "src/content_styles.css" -x "src/popup_styles.css" 1>/dev/null
+cd firefox && zip -r "../fediact-$1-firefox.zip" ./ -x "src/background.js" -x "src/inject.js" -x "src/popup.js" -x "src/content_styles.css" -x "src/popup_styles.css" 1>/dev/null
